9dd43883ec2b4e5cc31c32afe2a3b4509f9ca231
Andrey Ioch <dird15@mail.ru>
2021-08-30 18:29:44 +0300

    added a test which check that the notificator called send method

--- tests/UnitTests/Curiosity.Notifications.EMail.UnitTests/EMailNotificationsChannel_Should.cs
+++ tests/UnitTests/Curiosity.Notifications.EMail.UnitTests/EMailNotificationsChannel_Should.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Curiosity.EMail;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +35,54 @@ namespace Curiosity.Notifications.EMail.UnitTests
 
             // Assert
             channel.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Notificator_SendAsync()
+        {
+            // arrange
+            // create sender mock
+            var isSenderCalled = false;
+
+            var senderMock = new Mock<IEMailSender>();
+            senderMock
+                .Setup(x => x.SendAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true)
+                .Callback(() =>
+                {
+                    isSenderCalled = true;
+                });
+
+            // add to Ioc only channel without post processors
+            var services = new ServiceCollection();
+            services.AddCuriosityEMailChannel();
+
+            // add mocks
+            services.AddSingleton(Mock.Of<ILogger<EmailNotificationChannel>>());
+            services.AddSingleton(Mock.Of<ILogger>());
+            services.AddSingleton(senderMock.Object);
+            services.AddSingleton<INotificationBuilder, TestBuilder>();
+
+            var sp = services.BuildServiceProvider();
+            await sp.GetService<EmailNotificationChannel>().StartAsync(default);
+
+            // act
+            await sp.GetService<INotificator>().NotifyAsync(new TestMetadata());
+
+            // assert
+            isSenderCalled.Should().BeTrue();
+        }
+
+        public class TestMetadata : INotificationMetadata
+        {
+        }
+
+        public class TestBuilder : EmailNotificationBuilderBase<TestMetadata>
+        {
+            protected override Task<IReadOnlyList<INotification>> BuildNotificationsAsync(TestMetadata metadata, CancellationToken cancellationToken = default)
+            {
+                return Task.FromResult((IReadOnlyList<INotification>) new INotification[] {new EmailNotification("email", "subject", "body")});
+            }
         }
     }
 }
