ea2c83ac2eef47dd8e4faa8a7a65a352b8b520bd
Andrey Ioch <dird15@mail.ru>
2021-09-15 12:01:49 +0300

    [Email] Added test log email sender

---
+++ src/Curiosity.EMail.TestLog/IoCExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Curiosity.EMail.TestLog
+{
+    public static class IoCExtensions
+    {
+        /// <summary>
+        /// Adds <see cref="TestLogEmailSender"/> to services.
+        /// </summary>
+        public static IServiceCollection AddTestLogEMailSender(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.TryAddSingleton<IEMailSender, TestLogEmailSender>();
+            return services;
+        }
+    }
+}
