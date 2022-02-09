e47ef9dfd94e914a142c70e7f23cdaaee6c92416
adil.mir <adil.mir@itgarden>
2022-02-09 18:29:01 +0300

    Added regex email validation

--- src/Curiosity.EMail/EmailHelper.cs
+++ src/Curiosity.EMail/EmailHelper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using MimeKit;
 
 namespace Curiosity.EMail
@@ -57,13 +58,21 @@ namespace Curiosity.EMail
 
             return String.Join(", ", emailsList);
         }
+        
+        /// <summary>
+        /// Regex pattern for email validation.
+        /// </summary>
+        private const string EmailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
         /// <summary>
         /// Checks that email address is valid.
         /// </summary>
         public static bool IsEmailValid(string? email)
         {
-            return MailboxAddress.TryParse(ParserOptions.Default, email ?? "", out _);
+            var mailboxResult = MailboxAddress.TryParse(ParserOptions.Default, email ?? "", out _);
+            var isRegexMatch = Regex.Match(email ?? "", EmailPattern).Success;
+
+            return mailboxResult && isRegexMatch;
         }
     }
 }
