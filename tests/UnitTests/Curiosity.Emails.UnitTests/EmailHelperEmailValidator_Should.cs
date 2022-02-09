1e0727e910cfed740a0ac6fc0fc9c9d07d4cdba3
Maxim Markelow <markelow.dev@gmail.com>
2022-02-09 20:19:04 +0300

    Improved email validation tests

---
+++ tests/UnitTests/Curiosity.Emails.UnitTests/EmailHelperEmailValidator_Should.cs
@@ -0,0 +1,24 @@
+﻿using Curiosity.EMail;
+using Xunit;
+
+namespace Curiosity.Emails.UnitTests
+{
+    /// <summary>
+    /// Unit tests for <see cref="EmailHelper"/> for method <see cref="EmailHelper.IsEmailValid"/>.
+    /// </summary>
+    public class EmailHelper_Should
+    {
+        [Theory]
+        [InlineData("john@siisltd.ru", true)]
+        [InlineData("test.test@-com", false)]
+        [InlineData("mail.mail-test", false)]
+        public void ValidateEmail(string testedEmail, bool expectedResult)
+        {
+            //act
+            var actualResult = EmailHelper.IsEmailValid(testedEmail);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
